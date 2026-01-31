674bf9035dd6fc2e95da6dd9b7d0ddff7ba5211b
Richard Gagg <r.gagg@outlook.com>
2026-01-31 11:21:30 +1100

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Richard Gagg on 31/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BipTheGuy
+//
+//  Created by Richard Gagg on 31/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
